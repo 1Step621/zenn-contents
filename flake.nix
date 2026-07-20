b92b97c121f417471f497e85b391184b228ae419
1Step621 <onestep621@gmail.com>
2026-07-20 16:32:08 +0900

    chore: add nix setup

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.zenn-cli
+            ];
+          };
+        }
+      );
+    };
+}
